b86706b4882b6cf1bc12893a445d593fa64e721a
dinuie <iesanudinu01@gmail.com>
2022-09-28 11:28:49 +0300

    more refractor for board

--- src/main/java/com/codecool/battleship/board/BoardFactory.java
+++ src/main/java/com/codecool/battleship/board/BoardFactory.java
@@ -1,28 +1,32 @@
 package com.codecool.battleship.board;
+
 import com.codecool.battleship.Player;
 import com.codecool.battleship.Input;
 import com.codecool.battleship.ship.Ship;
+import com.codecool.battleship.square.Square;
+import com.codecool.battleship.square.SquareStatus;
+
+import java.util.List;
 
 public class BoardFactory {
-    public void placement(Player player, Input input) {
-        ships = player.ships;
+    public void manualPlacement(Player player, Board board, Input input) {
+        List<Ship> ships = player.getShips();
+        Square[][] ocean = board.getOcean();
         for (Ship ship : ships) {
             int[] startingCoord = input.coordInput();
-            if (isValidCoord(startingCoord)) {
-                if (ship.location.length = 1) {
-                    ship.location[0].x = startingCoord[0];
-                    ship.location[1].x = startingCoord[1];
-                }
+            if (isValidCoord(startingCoord, ocean) && isEmptyField(startingCoord, ocean)) {
+
             }
         }
     }
 
-    public boolean isValidCoord(int[] inputCoord) {
-        for (Square square : board.ocean) {
-            if (square.x = inputCoord[0] || square.y = inputCoord[1]) {
-                return true;
-            }
-        }
-        return false;
+    public boolean isValidCoord(int[] inputCoord, Square[][] ocean) {
+        int length = ocean.length;
+        return 0 <= inputCoord[0] && 0 <= inputCoord[1] && inputCoord[0] <= length && inputCoord[1] <= length;
+    }
+
+    public boolean isEmptyField(int[] inputCoord, Square[][] ocean){
+        Square targetSquare = ocean[inputCoord[0]][inputCoord[1]];
+        return (targetSquare.getSquareStatus() == SquareStatus.EMPTY);
     }
 }
